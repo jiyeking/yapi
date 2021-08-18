0ff80beef82cc5acf13292e3dd68ba5fa6a55789
jiyeking <jiyeking1@gmail.com>
2021-08-18 12:20:49 +0800

    Update sandbox.js
    解决在镜像中启动服务报错：Error: EROFS: read-only file system, mkdir '/sys/fs/cgroup/cpu/safeify'

--- server/utils/sandbox.js
+++ server/utils/sandbox.js
@@ -4,7 +4,8 @@ module.exports = async function sandboxFn(context, script) {
     // 创建 safeify 实例
     const safeVm = new Safeify({
         timeout: 3000,
-        asyncTimeout: 60000
+        asyncTimeout: 60000,
+        unrestricted: true  // 解决在镜像中启动服务报错：Error: EROFS: read-only file system, mkdir '/sys/fs/cgroup/cpu/safeify'
     })
 
     // 执行动态代码
